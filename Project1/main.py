81a01a299312e40fd455d037473a007309f6c379
TEONA-CHRISTIANA BOBIC <99075325+TeonaB@users.noreply.github.com>
2023-03-26 20:35:44 +0300

    Cod final DFA

--- Project1/main.py
+++ Project1/main.py
@@ -1,5 +1,5 @@
 with open("automat.txt", "r") as f:
-    stare_initiala = 'qo'
+    stare_initiala = 'q0'
     tranzitii = {}
     stari_acceptate = []
     stari_finale = []
@@ -25,10 +25,34 @@ with open("automat.txt", "r") as f:
         stari_finale = set(stari_finale)
         stari_finale = list(stari_finale)
 
-print("Cuvantul este")
-print(cuvant)
-print("starile finale")
-print(stari_finale)
-print("stari sunt:")
-print(stari_acceptate)
-print(tranzitii)
+def DFA(cuvant):
+    drumul_parcurs = []
+    stare_curenta = stare_initiala
+    cuvant = list(cuvant[0])
+    for litera in cuvant:
+        if (stare_curenta, litera) not in tranzitii:
+            return False
+        drumul_parcurs.append(stare_curenta)
+        stare_curenta = tranzitii[(stare_curenta, litera)]
+    if stare_curenta in stari_finale:
+        drumul_parcurs.append(stare_curenta)
+        return drumul_parcurs
+    else:
+        return False
+
+with open("final.txt", "w") as f:
+    if cuvant == []:
+        if 'q0' in stari_finale:
+            f.write("Acceptat")
+        else:
+            f.write("Neacceptat")
+    else:
+        if DFA(cuvant) == False:
+            f.write("Neacceptat")
+        else:
+            f.write("Acceptat")
+            f.write("\n")
+            raspuns=DFA(cuvant)
+            for r in raspuns:
+                f.write(r)
+                f.write(" ")
