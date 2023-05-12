6b331338efa0d367bc5219050c21cbbaa3dec61e
TEONA-CHRISTIANA BOBIC <99075325+TeonaB@users.noreply.github.com>
2023-05-12 20:46:46 +0300

    Update and rename main.py to Project3/main.py

--- Project3/main.py
+++ Project3/main.py
@@ -1,11 +1,11 @@
-def read_grammar(gramatica_fisier):
+def citire(gramatica_fisier):
     gramatica = []
     with open(gramatica_fisier) as file:
         for line in file:
             gramatica.append(line.strip())
     return gramatica
 
-def verificaCuvant(gramatica, simbol, cuvant):
+def VerificaCuvant(gramatica, simbol, cuvant):
     if len(cuvant) == 0:
         for regula in gramatica:
             if regula[0] == simbol and regula[2] == "x":
@@ -17,17 +17,17 @@ def verificaCuvant(gramatica, simbol, cuvant):
                 return True
     for regula in gramatica:
         if regula[0] == simbol and regula[2] == cuvant[0] and len(regula)>3:
-            if verificaCuvant(gramatica, regula[4], cuvant[1:]):
+            if VerificaCuvant(gramatica, regula[4], cuvant[1:]):
                 return True
     return False
 
-gramatica=read_grammar("gramatica.in")
+gramatica=citire("gramatica.in")
 simbol = 'S'
 with open("date.out","w") as file2:
     with open("cuvinte.in","r") as file:
         for line in file:
             cuvant = line.strip()
-            if verificaCuvant(gramatica, simbol, cuvant):
+            if VerificaCuvant(gramatica, simbol, cuvant):
                 file2.write(f"Cuvantul {cuvant} este acceptat.\n")
             else:
                 file2.write(f"Cuvantul {cuvant} nu este acceptat.\n")
